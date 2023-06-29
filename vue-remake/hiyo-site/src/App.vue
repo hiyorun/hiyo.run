8d8e370ed784e57704e81d0544eb8ba914f42fb5
hiyorun <hiyorrun@e.email>
2023-06-29 20:52:28 +0700

    feat: transition on first load

--- vue-remake/hiyo-site/src/App.vue
+++ vue-remake/hiyo-site/src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { onMounted, reactive } from 'vue';
+
+const firstTake = reactive({
+  initiated: false
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    firstTake.initiated = true
+  }, 1000)
+})
 </script>
 <template>
-  <router-view></router-view>
+  <router-view :class="[firstTake.initiated ? 'smoothing opacity-1' : 'opacity-0']"></router-view>
 </template>
