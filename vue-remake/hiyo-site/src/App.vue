a1bcd68c558a46f5d0e0266f750a6c18a20c55c2
hiyorun <hiyorrun@e.email>
2023-06-29 23:32:42 +0700

    feat: mock loading dialog

--- vue-remake/hiyo-site/src/App.vue
+++ vue-remake/hiyo-site/src/App.vue
@@ -1,16 +1,31 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
+import { usePrompt } from './states/prompts.js'
+import LoadingIndicator from './components/err-load/LoadingIndicator.vue'
 
 const firstTake = reactive({
   initiated: false
-})
+}),
+  prompt = usePrompt()
 
 onMounted(() => {
+  getPrompts()
   setTimeout(() => {
     firstTake.initiated = true
   }, 1000)
 })
+
+function getPrompts() {
+  fetch(
+    "https://raw.githubusercontent.com/hiyorun/site-prompts/main/prompts.json"
+  )
+    .then((response) => response.json())
+    .then((data) => prompt.storePrompts(data.prompts))
+    .catch((e) => console.error("error at get prompts:", e))
+    .finally(() => prompt.choosePrompt)
+}
 </script>
 <template>
+  <LoadingIndicator class="absolute m-3 z-50"/>
   <router-view :class="[firstTake.initiated ? 'smoothing opacity-1' : 'opacity-0']"></router-view>
 </template>
---
+++ vue-remake/hiyo-site/src/components/err-load/LoadingIndicator.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { usePrompt } from '@/states/prompts.js'
+import { reactive } from 'vue';
+const prompt = usePrompt()
+</script>
+<template>
+  <div class="text-arisu-900 dark:text-arisu-100 ">
+    <div
+      class="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">  
+    </div>
+    <span class="pl-3">{{ prompt.getPrompt }}</span>
+  </div>
+</template>
+<style scoped></style>
