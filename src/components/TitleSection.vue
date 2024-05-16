7e22d3c31fe52af5f65993cc8bf1ae2dca22c669
hiyorun <hiyorrun@e.email>
2024-05-17 01:04:08 +0700

    feat: added wave animations

--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -6,6 +6,10 @@ import Pixiv from '@/assets/imgs/icons/Pixiv.vue';
 import Reddit from '@/assets/imgs/icons/Reddit.vue';
 import Twitter from '@/assets/imgs/icons/Twitter.vue';
 import Misskey from '@/assets/imgs/icons/Misskey.vue';
+import Waves from './Waves.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const busy = useBusy()
 const socials = [
@@ -31,6 +35,10 @@ const socials = [
     },
 ]
 
+function toProject(){
+    router.replace("/#project")
+}
+
 onMounted(() => {
     busy.setBusy(false)
     console.log("mounted")
@@ -39,7 +47,7 @@ onMounted(() => {
 
 <template>
     <div
-        class="min-h-full mx-6 flex flex-col justify-around md:items-center md:text-center md:max-w-screen-md md:mx-auto">
+        class="h-screen mx-6 flex flex-col justify-around md:items-center md:text-center md:max-w-screen-md md:mx-auto">
         <div><!---spacer--></div>
         <div class="flex flex-col gap-2">
             <h1 class="text-4xl font-extrabold">
@@ -51,13 +59,14 @@ onMounted(() => {
             </div>
             <div class="flex gap-2 md:justify-center">
                 <a v-for="social in socials" :href="social.link" :title="social.name"
-                    class="bg-arisu-700 rounded-full p-3 text-arisu-100">
+                    class="transition-all duration-500 bg-arisu-700 hover:bg-arisu-600 dark:bg-arisu-100 hover:dark:bg-arisu-200 rounded-full p-3 text-arisu-100 dark:text-arisu-700">
                     <component :is="social.icon" class="w-7 h-7 flex items-center" />
                 </a>
             </div>
         </div>
-        <button>
-            <ArrowDownCircleIcon class="h-10 w-full" />
+        <button class="h-10 w-10 hover:w-12 hover:h-12 transition-all duration-500" @click="toProject()">
+            <ArrowDownCircleIcon/>
         </button>
     </div>
+    <Waves class="absolute bottom-0 right-0" />
 </template>
