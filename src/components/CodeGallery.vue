1772d4e3fd7bd656c2dd4e9a594a6238252b1900
hiyorun <hiyorrun@e.email>
2024-06-09 02:18:03 +0700

    fix: prevent click event to propagate to router-link

--- src/components/CodeGallery.vue
+++ src/components/CodeGallery.vue
@@ -22,7 +22,7 @@ function returnDateString(dateString) {
                 <i :class="[`devicon-${stack}-plain`]" class="text-xl"></i>
             </div>
             <span class="grow"></span>
-            <Tooltip msg="Open Site" v-if="props.data.attributes.details.links">
+            <Tooltip msg="Open Site" v-if="props.data.attributes.details.links" @click.stop>
                 <a :href="props.data.attributes.details.links" target="_blank" rel="noreferrer noopener"
                     class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900 hover:dark:bg-kikyou-800 text-kikyou-900 dark:text-kikyou-50 no-underline rounded-full flex items-center justify-center w-12 h-12">
                     <span class="material-symbols-outlined">
@@ -30,7 +30,7 @@ function returnDateString(dateString) {
                     </span>
                 </a>
             </Tooltip>
-            <Tooltip msg="Repository">
+            <Tooltip msg="Repository" @click.stop>
                 <a :href="props.data.attributes.git_repo" target="_blank" rel="noreferrer noopener"
                     class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900 hover:dark:bg-kikyou-800 text-kikyou-900 dark:text-kikyou-50 no-underline rounded-full flex items-center justify-center w-12 h-12">
                     <span class="material-symbols-outlined">
