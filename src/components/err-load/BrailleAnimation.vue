cbe4e2b96068e14f730d076215996a664e15194a
hiyorun <hiyorrun@e.email>
2024-08-02 10:28:43 +0700

    feat: new loading animation

---
+++ src/components/err-load/BrailleAnimation.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const brailles = [
+    '⡿',
+    '⣟',
+    '⣯',
+    '⣷',
+    '⣾',
+    '⣽',
+    '⣻',
+    '⢿'
+]
+const current = ref(0)
+const props = defineProps({
+    speed: {
+        type: Number,
+        default: 100
+    }
+})
+
+onMounted(async () => {
+    setInterval(() => {
+        current.value += 1
+        if (current.value >= brailles.length) {
+            current.value = 0
+        }
+    }, props.speed)
+})
+</script>
+<template>
+    <span class=" text-xl">{{ brailles[current] }}</span>
+</template>
--- src/components/err-load/LoadingIndicator.vue
+++ src/components/err-load/LoadingIndicator.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { shufflePrompt } from '@/uses/usePrompts.js';
-import { usePrompt } from '@/states/prompts.js'
-import { useBusy } from '@/states/busy.js'
+import { shufflePrompt } from '@/uses/usePrompts';
+import { usePrompt } from '@/states/prompts'
 import { onMounted } from 'vue';
+import BrailleAnimation from './BrailleAnimation.vue';
+import { useViewport } from '@/uses/useViewport';
+import { useBusy } from '@/states/busy';
 
-const prompt = usePrompt(),
-  busy = useBusy()
+const prompt = usePrompt()
+const viewport = useViewport()
+const busy = useBusy()
 
 onMounted(() => {
   getPrompts()
@@ -19,14 +22,23 @@ function getPrompts() {
     .then((data) => prompt.storePrompts(data.prompts))
     .catch((e) => console.error("error at get prompts:", e))
 }
+
+function truncatePrompts() {
+  if (viewport.media.isSm) return ""
+  const maxLen = 30
+  const prompt = shufflePrompt();
+  if (prompt.length > maxLen) {
+    const withEllipsis = prompt.substring(0, maxLen) + "..."
+    return withEllipsis
+  }
+  return prompt
+}
+
 </script>
 <template>
-  <div v-if="busy.isBusy">
-    <div
-      class="inline-block h-3 w-3 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-      role="status">
-    </div>
-    <span class="overflow-hidden text-sm text-nowrap">{{ shufflePrompt() }}</span>
+  <div v-if="busy.isBusy" class="flex gap-2 items-baseline">
+    <BrailleAnimation />
+    <span class="overflow-hidden text-sm text-nowrap">{{ truncatePrompts() }}</span>
   </div>
 </template>
 <style scoped></style>
