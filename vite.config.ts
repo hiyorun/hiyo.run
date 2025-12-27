18f066970507fac4eff133cc5d9c24d6e18a1ca5
hiyorun <hiyorrun@e.email>
2025-12-28 00:24:06 +0700

    chore: config migrations to TS

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  build:{
-    cssMinify: 'lightningcss'
+  build: {
+    cssMinify: "lightningcss",
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
