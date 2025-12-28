4604efe77a4f088357bacd39b7d45c4094dd27ec
hiyorun <hiyorrun@e.email>
2025-12-28 23:38:14 +0700

    feat: new layouts for code pages and components

--- src/components/CodeGallery.vue
+++ src/components/CodeGallery.vue
@@ -27,58 +27,66 @@
   <a
     @click="navigator({ name: 'code-details', params: { id: props.data.id } })"
     class="transition-colors duration-200 bg-kikyou-200 hover:bg-kikyou-300 dark:bg-kikyou-900
-      hover:dark:bg-kikyou-700 text-kikyou-900 dark:text-kikyou-50 shadow-xl rounded-2xl p-4 flex
+      hover:dark:bg-kikyou-700 text-kikyou-900 dark:text-kikyou-50 shadow-xl rounded-2xl p-5 flex
       flex-col gap-2 min-h-52 cursor-pointer"
   >
     <span class="opacity-50 text-xs">{{ returnDateString(data.date.toLocaleString()) }}</span>
     <span class="text-2xl font-bold">{{ props.data.title }}</span>
     <span class="grow">{{ props.data.description }}</span>
-    <div
-      class="flex items-center gap-2"
-    >
-      <template v-for="detail in props.data._relations?.codeDetails" :key="detail.id">
-        <div
-          :key="stack"
-          v-for="stack in detail.stacks"
-        >
-          <i
-            :class="[`devicon-${stack}-plain`]"
-            class="text-xl"
-          ></i>
-        </div>
-        <span class="grow"></span>
-        <Tooltip
-          msg="Open Site"
-          v-if="detail.link"
-          @click.stop
-        >
-          <a
-            :href="detail.link"
-            target="_blank"
-            rel="noreferrer noopener"
-            class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900
+    <div class="flex gap-2 text-sm">
+      <a
+        class="transition-colors duration-200 bg-kikyou-700 hover:bg-kikyou-600 dark:bg-kikyou-50
+          hover:dark:bg-kikyou-200 rounded-full px-2 text-kikyou-50 dark:text-kikyou-700"
+        v-for="(license, index) in props.data.licenses"
+        :key="index"
+        :href="license.link"
+      >
+        {{ license.name }}
+      </a>
+    </div>
+    <div class="flex items-center gap-2">
+      <div
+        :key="stack"
+        v-for="stack in props.data.stacks"
+      >
+        <i
+          :class="[`devicon-${stack}-plain`]"
+          class="bg-kikyou-700 dark:bg-kikyou-50 rounded-full text-kikyou-50 dark:text-kikyou-700
+            text-xl p-2"
+        ></i>
+      </div>
+      <span class="grow"></span>
+      <Tooltip
+        msg="Open Site"
+        v-if="props.data.link"
+        @click.stop
+      >
+        <a
+          :href="props.data.link"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900
             hover:dark:bg-kikyou-800 text-kikyou-900 dark:text-kikyou-50 no-underline rounded-full
             flex items-center justify-center w-12 h-12"
-          >
-            <span class="material-symbols-outlined"> open_in_new </span>
-          </a>
-        </Tooltip>
-        <Tooltip
-          msg="Repository"
-          @click.stop
         >
-          <a
-            :href="props.data.repository"
-            target="_blank"
-            rel="noreferrer noopener"
-            class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900
+          <span class="material-symbols-outlined"> open_in_new </span>
+        </a>
+      </Tooltip>
+      <Tooltip
+        msg="Repository"
+        @click.stop
+      >
+        <a
+          :href="props.data.repository"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="transition-colors duration-200 bg-kikyou-100 hover:bg-kikyou-200 dark:bg-kikyou-900
             hover:dark:bg-kikyou-800 text-kikyou-900 dark:text-kikyou-50 no-underline rounded-full
             flex items-center justify-center w-12 h-12"
-          >
-            <span class="material-symbols-outlined"> code </span>
-          </a>
-        </Tooltip>
-      </template>
+        >
+          <span class="material-symbols-outlined"> code </span>
+        </a>
+      </Tooltip>
     </div>
   </a>
 </template>
--- src/pages/CodeDetails.vue
+++ src/pages/CodeDetails.vue
@@ -33,34 +33,80 @@
       <span class="material-symbols-outlined text-sm"> arrow_back_ios </span>
       Back
     </RouterLink>
+    <div class="flex flex-col gap-3">
+      <span class="opacity-50 text-sm">{{ returnDateString(data?.date ? data.date : '') }}</span>
+      <span class="text-4xl tracking-tighter font-bold">
+        {{ data?.title }}
+        <span
+          class="font-medium"
+          v-if="data?.description !== ''"
+        >
+          | <span class="text-2xl">{{ data?.description }}</span>
+        </span>
+      </span>
+      <div class="flex gap-2 text-sm">
+        <a
+          class="transition-colors duration-200 bg-kikyou-700 hover:bg-kikyou-600 dark:bg-kikyou-50
+            hover:dark:bg-kikyou-200 rounded-full px-2 text-kikyou-50 dark:text-kikyou-700"
+          v-for="(license, index) in data?.licenses"
+          :key="index"
+          :href="license.link"
+        >
+          {{ license.name }}
+        </a>
+      </div>
+      <div class="flex items-baseline gap-2">
+        <i
+          v-for="stack in data?.stacks"
+          :class="[`devicon-${stack}-plain`]"
+          :key="stack"
+          class="bg-kikyou-700 dark:bg-kikyou-50 rounded-full text-kikyou-50 dark:text-kikyou-700
+            text-xl p-2"
+        ></i>
+      </div>
+    </div>
     <template
       :key="detail.id"
       v-for="detail in data?._relations?.codeDetails"
     >
-      <div class="flex flex-col gap-2">
-        <span class="opacity-50 text-sm">{{ returnDateString(data?.date ? data.date : '') }}</span>
-        <span class="text-4xl tracking-tighter font-bold">
-          {{ data?.title }}
-        </span>
-        <span class="">{{ data?.description }}</span>
-        <div class="flex items-baseline gap-2">
-          <i
-            v-for="stack in detail.stacks"
-            :class="[`devicon-${stack}-plain`]"
-            :key="stack"
-            class="text-xl"
-          ></i>
-        </div>
-      </div>
       <div class="mt-12 flex flex-col gap-6">
         <template
           :key="section.title"
           v-for="section in detail.sections"
         >
-          <span>
-            <div class="text-2xl font-bold mb-2">{{ section.title }}</div>
-            {{ section.body }}
-          </span>
+          <div class="flex flex-col gap-3">
+            <div class="text-3xl font-bold">{{ section.title }}</div>
+            <template
+              v-for="(body, index) in section.body"
+              :key="index"
+            >
+              <p v-if="body.type === 'p'">
+                {{ body.value }}
+              </p>
+              <ol
+                class="list-decimal"
+                v-if="body.type === 'ol'"
+              >
+                <li
+                  v-for="(val, index) in body.value"
+                  :key="index"
+                >
+                  {{ val }}
+                </li>
+              </ol>
+              <ul
+                class="list-disc px-5"
+                v-if="body.type === 'ul'"
+              >
+                <li
+                  v-for="(val, index) in body.value"
+                  :key="index"
+                >
+                  {{ val }}
+                </li>
+              </ul>
+            </template>
+          </div>
         </template>
       </div>
     </template>
